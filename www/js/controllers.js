07a7da48c3f31d68ad2bca222cb8a86815cf863f
scalka <calka.sylwia@gmail.com>
2016-05-30 09:15:36 +0200

    before storage addition

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,42 @@
 angular.module('app.controllers', [])
-  
+
+.controller('MainCtrl', function($scope){
+
+})
+
 .controller('cameraCtrl', function($scope) {
 
 })
-   
+
 .controller('newEntryCtrl', function($scope) {
 
+  $scope.entries = [];
+
+  $scope.addEntry = function(){
+
+    $scope.entries.push($scope.titleadded);
+    $scope.entries.push($scope.content);
+
+    $scope.titleadded='';
+    $scope.content='';
+  }
+    $scope.removeEntry = function (index) {
+      $scope.entries.splice(index, 1);
+    };
 })
-   
+
 .controller('calendarCtrl', function($scope) {
 
 })
-         
+
 .controller('entryDetailCtrl', function($scope) {
 
 })
-   
+
 .controller('profileCtrl', function($scope) {
 
 })
-   
+
 .controller('settingsCtrl', function($scope) {
 
 })
- 
