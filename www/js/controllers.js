7ecc332d4394c1ddc732b0a2a5c9c60a1c8005e7
scalka <calka.sylwia@gmail.com>
2016-06-01 18:40:46 +0200

    before playing with routes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,8 +4,36 @@ angular.module('app.controllers', [])
 
 })
 
-.controller('cameraCtrl', function($scope) {
+.controller('cameraCtrl', function($scope, $base64, Camera, $localStorage, $state, $sessionStorage) {
 
+      $scope.$storage = $localStorage;
+
+      $scope.getPhoto = function() {
+            console.log("btn clicked");
+            //first we define a var to set the settings we use calling the cordova camera,
+            var cameraSettings = {
+              sourceType: 1, //navigator.camera.PictureSourceType.CAMERA,
+              destinationType: 0, //navigator.camera.DestinationType.DATA_URL, // very importend!!! to get base64 and no link NOTE: mybe cause out of memory error after a while
+              quality: 75,
+              targetWidth: 320,
+              targetHeight: 320,
+              saveToPhotoAlbum: true,
+            };
+          //calling our service with asynchronously runs the cordova camera plugin
+         Camera.getPicture(cameraSettings).then(function(imageData) {
+            //adding the phone number and pasing the object to json
+            var image= {
+                "imageData":imageData,
+                "timestamp": Date.parse(Date())
+              };
+            //store localy now
+            storage.addOwnImage(image);
+         }, function(err) {
+           console.log(err);
+          //this function dosnt even get called, have to make a cetch outside before
+          });
+      };
+    // $scope.ownImages = $localStorage.ownImages;
 })
 
 .controller('newEntryCtrl', function($scope,localStorageService, $state) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,28 @@
 angular.module('app.services', [])
-
-.factory('BlankFactory', [function(){
+.service('BlankService', [function(){
 
 }])
 
-.service('BlankService', [function(){
+.factory('Camera', ['$q', function($q) {
+ // creating camera factory calls codova plugin to load native camera app
+ //$q is used to give the controller a promise, check out the promise Api of angular for more details
+ //$q and deferred is asynchronous programing and try, catch, throw is synchronous programming
+ //== A service that helps you run functions asynchronously, and use their return values (or exceptions) when they are done processing
+  return {
+    getPicture: function(options) {
+      // this is the same as try and catch, just asynchronous
+      var deferred = $q.defer();
+      navigator.camera.getPicture(function(result) {
+        //calling codova plugin
+        deferred.resolve(result);
+      }, function(err) {
+        deferred.reject(err);
+      }, options);
 
+      //handling the results
+      return deferred.promise;
+      //return a promise
+    }
+
+  }
 }]);
-
